2fb6b8f4a82488efd62e8875da7f018c2a923de0
koffi <ouandan.koffi@gmail.com>
2026-04-15 16:55:01 +0200

    Fichier ajout dans le panier

---
+++ scripts/cart.js
@@ -0,0 +1,46 @@
+
+
+const departure = document.querySelector(".departure")
+const arrival = document.querySelector(".arrival")
+const hour = document.querySelector(".hour")
+const price = document.querySelector(".price")
+const btnDelete = document.querySelector(".btnDelete")
+const totalPrice = document.querySelector(".totalPrice")
+const btnPourchase = document.querySelector(".btnPourchase")
+const tripRows = document.querySelector(".container-trajet")
+
+
+fetch('http://localhost:3000/cart')
+.then(response => response.json())
+.then(data =>{
+    console.log(data.cart)
+   tripRows.innerHTML = ""
+   let montPrice = 0
+
+
+    for (let i = 0; i<data.cart.length; i++){
+        for(let trip of data.cart[i].trips){
+        console.log(trip[0])
+
+        montPrice = montPrice + trip.price
+
+        let date = new Date(trip.date)
+        const tripDateHour = date.getHours()
+        const tripDateMinutes = date.getMinutes()
+        const tripRow = document.createElement("div")
+
+        tripRow.classList.add('rowTrips')
+        tripRow.innerHTML = `
+        <p><span>${trip.departure}</span> > <span>${trip.arrival}</span></p>
+        <p>${tripDateHour}:${tripDateMinutes}</p>
+        <p>${trip.price} €</p>
+        <button>X</button>
+        `;
+        tripRows.appendChild(tripRow);
+    }
+    totalPrice.textContent = montPrice
+    console.log(montPrice)
+}
+})
+
+// Supprimer un voyage dans le panier
