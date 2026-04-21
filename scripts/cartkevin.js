317f6a41422aed0abb1972598659348d88e05c92
kd-com <contact@kd-com.fr>
2026-04-21 17:18:31 +0200

    UPD - cart page with delete trip

--- scripts/cartkevin.js
+++ scripts/cartkevin.js
@@ -1,12 +1,15 @@
+
+
+
 const cart = document.getElementById('updatedCart');
 fetch('http://localhost:3000/cart')
 .then(response => response.json())
 .then(data => {
     if(data.result === true) {
         cart.innerHTML = '';
+        let sumPrice = 0;
         for(trip of data.cart.trips) {
-            let totalPrice = 0;
-            totalPrice = totalPrice + trip.price;
+            sumPrice = sumPrice + trip.price;
             let date = new Date(trip.date);
             const tripDateHour = date.getHours();
             const tripDateMinute = date.getMinutes();
@@ -25,7 +28,21 @@ fetch('http://localhost:3000/cart')
             const hours = document.createElement('div')
             hours.textContent = `${tripDateHour}:${tripDateMinute}`
             const price = document.createElement('div')
+            price.classList.add('price')
             price.textContent = `${trip.price}€`;
+            const btnDelete = document.createElement('button')
+            btnDelete.setAttribute('id', `${trip._id}`);
+            btnDelete.textContent= 'X';
+            btnDelete.classList.add('deletebtn');
+            btnDelete.addEventListener('click', function() {
+                fetch(`http://localhost:3000/cart/${this.id}`, {method: 'DELETE'})
+                .then(response => response.json())
+                .then(data => {
+                    if(data.result) {
+                        tripRow.remove();
+                    }
+                })
+            })
 
             cart.appendChild(tripRow);
             tripRow.appendChild(divDepAr);
@@ -34,6 +51,22 @@ fetch('http://localhost:3000/cart')
             divDepAr.appendChild(arrival);
             tripRow.appendChild(hours);
             tripRow.appendChild(price);
+            tripRow.appendChild(btnDelete);
+
+
+            
         }
+        const totalRow = document.createElement('div');
+            totalRow.classList.add('totalrow');
+            const priceTotal = document.createElement('p');
+            priceTotal.classList.add('pricetotal');
+            priceTotal.textContent = `TOTAL : ${sumPrice}€`
+            const purchasebtn = document.createElement('button');
+            purchasebtn.classList.add('purchasebtn');
+            purchasebtn.textContent='Purchase'
+            cart.appendChild(totalRow)
+            totalRow.appendChild(priceTotal);
+            totalRow.appendChild(purchasebtn);
     }
 })
+deleteTrajet()
